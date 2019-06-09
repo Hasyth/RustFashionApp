a6ada124038def0b8efe0aa1bff2f723093b05e1
Hasyth <damithhasitha81@gmail.com>
2019-06-09 13:01:54 +0530

    Completed up to product display

--- app/src/main/java/com/example/rustfashionapp/Product_Package/ProductDetails.java
+++ app/src/main/java/com/example/rustfashionapp/Product_Package/ProductDetails.java
@@ -1,30 +1,30 @@
 package com.example.rustfashionapp.Product_Package;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.widget.RecyclerView;
+        import android.support.v7.widget.Toolbar;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageView;
+        import android.widget.ScrollView;
+        import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.example.rustfashionapp.AppSingleton;
-import com.example.rustfashionapp.R;
-import com.google.gson.Gson;
+        import com.android.volley.Request;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.JsonObjectRequest;
+        import com.bumptech.glide.Glide;
+        import com.bumptech.glide.request.RequestOptions;
+        import com.example.rustfashionapp.AppSingleton;
+        import com.example.rustfashionapp.R;
+        import com.google.gson.Gson;
 
-import org.json.JSONObject;
+        import org.json.JSONObject;
 
 public class ProductDetails extends AppCompatActivity {
     TextView prodName;
@@ -61,9 +61,9 @@ public class ProductDetails extends AppCompatActivity {
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-               // if(menuItem.getItemId()==R.id.refresh){
-                    getProduct(prodId);
-               // }
+                // if(menuItem.getItemId()==R.id.refresh){
+                getProduct(prodId);
+                // }
                 return false;
             }
         });
